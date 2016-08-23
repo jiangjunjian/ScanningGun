4d30669e240d7ee66a537abb3dcbadd6ce9fb771
jiahaohk <jiahaohk@gmail.com>
2016-08-23 23:21:05 +0800

    sdjfslaf

---
+++ ScanningGun/FrmMain.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace ScanningGun
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbtiaoma = new System.Windows.Forms.TextBox();
+            this.tbname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbkucun = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(137, 61);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 104);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(137, 104);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 140);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "条形码：";
+            // 
+            // tbtiaoma
+            // 
+            this.tbtiaoma.Location = new System.Drawing.Point(152, 263);
+            this.tbtiaoma.Name = "tbtiaoma";
+            this.tbtiaoma.Size = new System.Drawing.Size(144, 21);
+            this.tbtiaoma.TabIndex = 8;
+            // 
+            // tbname
+            // 
+            this.tbname.Location = new System.Drawing.Point(152, 302);
+            this.tbname.Name = "tbname";
+            this.tbname.Size = new System.Drawing.Size(144, 21);
+            this.tbname.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "名称：";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(25, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "1";
+            this.radioButton1.Text = "电容";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 350);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "分类：";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(97, 20);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Tag = "2";
+            this.radioButton2.Text = "电阻";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(152, 340);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 50);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "分类";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "库存：";
+            // 
+            // tbkucun
+            // 
+            this.tbkucun.Location = new System.Drawing.Point(152, 416);
+            this.tbkucun.Name = "tbkucun";
+            this.tbkucun.Size = new System.Drawing.Size(144, 21);
+            this.tbkucun.TabIndex = 16;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 549);
+            this.Controls.Add(this.tbkucun);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbname);
+            this.Controls.Add(this.tbtiaoma);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbtiaoma;
+        private System.Windows.Forms.TextBox tbname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbkucun;
+    }
+}
+
---
+++ ScanningGun/FrmMain.cs
@@ -0,0 +1,140 @@
+﻿using ScanningGun.entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScanningGun
+{
+    public partial class FrmMain : Form
+    {
+        InventoryContext db = new InventoryContext();
+        BardCodeHooK BarCode = new BardCodeHooK();
+        public FrmMain()
+        {
+            InitializeComponent();
+            BarCode.BarCodeEvent += new BardCodeHooK.BardCodeDeletegate(BarCode_BarCodeEvent);
+        }
+
+        private delegate void ShowInfoDelegate(BardCodeHooK.BarCodes barCode);
+        private void ShowInfo(BardCodeHooK.BarCodes barCode)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new ShowInfoDelegate(ShowInfo), new object[] { barCode });
+            }
+            else
+            {
+
+                tbtiaoma.Text = barCode.IsValid ? barCode.BarCode : "";
+                if (!string.IsNullOrEmpty(tbtiaoma.Text))
+                {
+                    var goodinfo = db.Goods.First(c => c.barCode == tbtiaoma.Text);
+
+                    if (goodinfo != null)
+                    {
+                        tbname.Text = goodinfo.Name;
+                        tbtiaoma.Text = goodinfo.barCode;
+                        tbkucun.Text = goodinfo.quanty.ToString();
+                        foreach (Control c in groupBox1.Controls)//遍历groupBox1中的所有空间
+                        {
+                            if (c is RadioButton)//判断该控件是不是RadioButton
+                            {
+                                RadioButton r = (RadioButton)c;//做转化
+                                if (r.Tag.ToString()==goodinfo.cateogory.ToString())
+                                {
+                                    r.Checked = true;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var newgood = new GoodsEntity
+                        {
+                            barCode = tbtiaoma.Text,
+                            Name = tbname.Text,
+                         quanty=int.Parse(tbkucun.Text)
+                        };
+
+                        foreach (Control c in groupBox1.Controls)//遍历groupBox1中的所有空间
+                        {
+                            if (c is RadioButton)//判断该控件是不是RadioButton
+                            {
+                                RadioButton r = (RadioButton)c;//做转化
+                                bool rbState = r.Checked;//得到Checked状态
+                                if (r.Checked)
+                                {
+                                    newgood.cateogory =int.Parse(r.Tag.ToString());
+                                }
+                            }
+                        }
+
+                        db.Goods.Add(newgood);
+                    }
+                }
+
+
+                ///// 触发扫描事件
+                //1.判断是否有录入
+                //2. 显示名称库存
+                // 3.填写库存 + -
+
+                textBox1.Text = barCode.KeyName;
+                textBox2.Text = barCode.VirtKey.ToString();
+                textBox3.Text = barCode.ScanCode.ToString();
+                textBox4.Text = barCode.Ascll.ToString();
+                textBox5.Text = barCode.Chr.ToString();
+                textBox6.Text = barCode.IsValid ? barCode.BarCode : "";//是否为扫描枪输入，如果为true则是 否则为键盘输入
+
+                textBox7.Text += barCode.KeyName;
+                //MessageBox.Show(barCode.IsValid.ToString());
+            }
+        }
+
+        //C#中判断扫描枪输入与键盘输入 
+
+        //Private DateTime _dt = DateTime.Now;  //定义一个成员函数用于保存每次的时间点
+        //private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    DateTime tempDt = DateTime.Now;          //保存按键按下时刻的时间点
+        //    TimeSpan ts = tempDt .Subtract(_dt);     //获取时间间隔
+        //    if (ts.Milliseconds > 50)                           //判断时间间隔，如果时间间隔大于50毫秒，则将TextBox清空
+        //        textBox1.Text = "";
+        //    dt = tempDt ;
+        //}
+
+
+
+        void BarCode_BarCodeEvent(BardCodeHooK.BarCodes barCode)
+        {
+            ShowInfo(barCode);
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            BarCode.Start();
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            BarCode.Stop();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox6.Text.Length > 0)
+            {
+                MessageBox.Show("条码长度：" + textBox6.Text.Length + "\n条码内容：" + textBox6.Text, "系统提示");
+            }
+        }
+
+
+    }
+}
